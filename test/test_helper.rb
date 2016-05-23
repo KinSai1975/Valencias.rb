fb932f748b751dc41fc2d4b4c32a3336945ff71e
KinSai1975 <acmilan9275@gmail.com>
2016-05-23 19:57:12 +0200

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,6 @@ libdir = File.dirname(File.dirname(__FILE__)) + '/lib'
 $LOAD_PATH.unshift libdir unless $LOAD_PATH.include?(libdir)
 
 require "rubygems"
-require "nancy"
+require "valencias"
 require "minitest/autorun"
 require "rack/test"
